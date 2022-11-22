ef7d6a234c0f6534b397738c2a20854816a88e08
Goetz Gaycken <goetz.gaycken@cern.ch>
2022-11-22 10:51:22 +0100

    Moved Stat helper to Utilities.

--- Examples/Io/Performance/ActsExamples/Io/Performance/CKFPerformanceWriter.cpp
+++ Examples/Io/Performance/ActsExamples/Io/Performance/CKFPerformanceWriter.cpp
@@ -38,27 +38,6 @@
 #include <TTree.h>
 #include <TVectorF.h>
 
-namespace ActsExamples {
-   std::ostream &operator<<(std::ostream &out, const Stat &a ) {
-      out <<           std::setw(14) << a.min() << " < "
-          <<           std::setw(14) << a.mean()
-          << " +- " << std::setw(14) << a.rms()
-          << " < "  << std::setw(14) << a.max()
-          << " / "  << std::setw(9)  << a.n();
-      if (!a.bins().empty()) {
-         out << std::endl;
-         out << a.lowerEdge() << ", " << a.lowerEdge()+a.binWidth() << ".." << a.upperEdge() << ": ";
-         out << std::setw(5) << a.bins()[0] << " |";
-         for (unsigned int bin_i=1; bin_i<a.bins().size()-1; ++bin_i) {
-            out << " " << std::setw(5) << a.bins()[bin_i];
-         }
-         out << " | " << std::setw(5) << a.bins().back() ;
-      }
-      return out;
-   }
-}
-
-
 ActsExamples::CKFPerformanceWriter::CKFPerformanceWriter(
     ActsExamples::CKFPerformanceWriter::Config cfg, Acts::Logging::Level lvl)
     : WriterT(cfg.inputTrajectories, "CKFPerformanceWriter", lvl),
